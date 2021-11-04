b908bbb5c29f7f65c0f41b7cdc31b38dc1232a72
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2021-11-04 22:51:28 +0800

    Changed raw-loader CodeBlock classnames with language- prefix to syntax hightlight to work, format changes to docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,147 +1,158 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 const math = require("remark-math");
-const katex = require("rehype-katex");
-module.exports = {
-  title: "Fortran Tutorial",
-  tagline: "Learning Fortran Programming",
-  url: "https://pranabdas.github.io",
-  baseUrl: "/fortran/", // must have a trailing "/"
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
-  organizationName: "pranabdas", // Usually your GitHub org/user name.
-  projectName: "fortran", // Usually your repo name.
-  themeConfig: {
-    prism: {
-      theme: require("prism-react-renderer/themes/github"),
-      darkTheme: require("prism-react-renderer/themes/nightOwl"),
-      additionalLanguages: ["docker", "fortran"],
-    },
-    hideableSidebar: true,
-    colorMode: {
-    //   defaultMode: "dark",
-    //   switchConfig: {
-    //     // darkIcon: '☾',
-    //     // lightIcon: "☀️"
-    //   },
-    //   respectPrefersColorScheme: true,
-    },
-    navbar: {
-      title: "Fortran",
-      hideOnScroll: true,
-      logo: {
-        alt: "Logo",
-        src: "img/favicon.ico",
-      },
-      items: [
-        {
-          to: "/",
-          label: "Docs",
-          position: "left",
+/** @type {import('@docusaurus/types').Config} */
+async function config() {
+  const katex = (await import("rehype-katex")).default;
+  return {
+    title: "Fortran Tutorial",
+    tagline: "Learning Fortran Programming",
+    url: "https://pranabdas.github.io",
+    baseUrl: "/fortran/", // must have a trailing "/"
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.ico",
+    organizationName: "pranabdas", // Usually your GitHub org/user name.
+    projectName: "fortran", // Usually your repo name.
+    themeConfig:
+      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+      ({
+        prism: {
+          theme: require("prism-react-renderer/themes/github"),
+          darkTheme: require("prism-react-renderer/themes/nightOwl"),
+          additionalLanguages: ["fortran", "docker"],
+        },
+        hideableSidebar: true,
+        colorMode: {
+          //   defaultMode: "dark",
+          //   switchConfig: {
+          //     // darkIcon: '☾',
+          //     // lightIcon: "☀️"
+          //   },
+          //   respectPrefersColorScheme: true,
+        },
+        navbar: {
+          title: "Fortran",
+          hideOnScroll: true,
+          logo: {
+            alt: "Logo",
+            src: "img/favicon.ico",
+          },
           items: [
             {
-              label: "Fortran Programming",
               to: "/",
+              label: "Docs",
+              position: "left",
+              items: [
+                {
+                  label: "Fortran Programming",
+                  to: "/",
+                  target: "_self",
+                },
+                {
+                  label: "~ ~ ~ Other Docs ~ ~ ~",
+                  to: "https://pranabdas.github.io/docs/",
+                  target: "_self",
+                },
+                {
+                  label: "ARPES Python Tools",
+                  to: "https://pranabdas.github.io/arpespythontools/",
+                  target: "_self",
+                },
+                {
+                  label: "Linux Tutorial",
+                  to: "https://pranabdas.github.io/linux/",
+                  target: "_self",
+                },
+                {
+                  label: "Machine Learning Notes",
+                  to: "https://pranabdas.github.io/machine-learning/",
+                  target: "_self",
+                },
+                {
+                  label: "OpenMX tutorial",
+                  to: "https://pranabdas.github.io/openmx/",
+                  target: "_self",
+                },
+                {
+                  label: "Python Tutorial",
+                  to: "https://pranabdas.github.io/python-tutorial/",
+                  target: "_self",
+                },
+                {
+                  label: "Quantum Espresso Tutorial",
+                  to: "https://pranabdas.github.io/espresso/",
+                  target: "_self",
+                },
+                {
+                  label: "SUV Python Tools",
+                  to: "https://pranabdas.github.io/suvtools/",
+                  target: "_self",
+                },
+                {
+                  label: "Condensed Matter Notes",
+                  href: "https://pranabdas.github.io/docs/condmat-notes/",
+                },
+                {
+                  label: "Javascript Tutorial",
+                  href: "https://pranabdas.github.io/docs/js-learning/",
+                },
+                {
+                  label: "SUV ARPES Manual",
+                  href: "https://pranabdas.github.io/docs/arpes-man/",
+                },
+              ],
+            },
+            {
+              to: "https://pranabdas.github.io",
+              label: "About me",
+              position: "left",
               target: "_self",
             },
             {
-              label: "~ ~ ~ Other Docs ~ ~ ~",
-              to: "https://pranabdas.github.io/docs/",
-              target: "_self",
-            },
-            {
-              label: "ARPES Python Tools",
-              to: "https://pranabdas.github.io/arpespythontools/",
-              target: "_self",
-            },
-            {
-              label: "Linux Tutorial",
-              to: "https://pranabdas.github.io/linux/",
-              target: "_self",
-            },
-            {
-              label: "Machine Learning Notes",
-              to: "https://pranabdas.github.io/machine-learning/",
-              target: "_self",
-            },
-            {
-              label: "OpenMX tutorial",
-              to: "https://pranabdas.github.io/openmx/",
-              target: "_self",
-            },
-            {
-              label: "Python Tutorial",
-              to: "https://pranabdas.github.io/python-tutorial/",
-              target: "_self",
-            },
-            {
-              label: "Quantum Espresso Tutorial",
-              to: "https://pranabdas.github.io/espresso/",
-              target: "_self",
-            },
-            {
-              label: "SUV Python Tools",
-              to: "https://pranabdas.github.io/suvtools/",
-              target: "_self",
-            },
-            {
-              label: "Condensed Matter Notes",
-              href: "https://pranabdas.github.io/docs/condmat-notes/",
-            },
-            {
-              label: "Javascript Tutorial",
-              href: "https://pranabdas.github.io/docs/js-learning/",
-            },
-            {
-              label: "SUV ARPES Manual",
-              href: "https://pranabdas.github.io/docs/arpes-suv-man/",
+              href: "https://github.com/pranabdas/fortran",
+              position: "right",
+              className: "header-github-link",
+              "aria-label": "GitHub repository",
             },
           ],
         },
-        {
-          to: "https://pranabdas.github.io",
-          label: "About me",
-          position: "left",
-          target: "_self",
-        },
-        {
-          href: "https://github.com/pranabdas/fortran",
-          position: "right",
-          className: "header-github-link",
-          "aria-label": "GitHub repository",
-        },
-      ],
-    },
-    footer: {
-      style: "dark",
-      copyright: `Copyright © ${new Date().getFullYear()} Pranab Das. All rights
+        footer: {
+          style: "dark",
+          copyright: `Copyright © ${new Date().getFullYear()} Pranab Das. All rights
       reserved.`,
-    },
-  },
-  presets: [
-    [
-      "@docusaurus/preset-classic",
+        },
+      }),
+
+    presets: [
+      [
+        "@docusaurus/preset-classic",
+        /** @type {import('@docusaurus/preset-classic').Options} */
+        ({
+          docs: {
+            sidebarPath: require.resolve("./sidebars.js"),
+            routeBasePath: "/",
+            // Please change this to your repo.
+            editUrl: "https://github.com/pranabdas/fortran/blob/master/",
+            remarkPlugins: [math],
+            rehypePlugins: [katex],
+          },
+          theme: {
+            customCss: require.resolve("./src/css/custom.css"),
+          },
+        }),
+      ],
+    ],
+
+    stylesheets: [
       {
-        docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          routeBasePath: "/",
-          // Please change this to your repo.
-          editUrl: "https://github.com/pranabdas/fortran/blob/master/",
-          remarkPlugins: [math],
-          rehypePlugins: [katex],
-        },
-        theme: {
-          customCss: require.resolve("./src/css/custom.css"),
-        },
+        href: "https://cdn.jsdelivr.net/npm/katex@0.15.2/dist/katex.min.css",
+        type: "text/css",
+        integrity:
+          "sha384-MlJdn/WNKDGXveldHDdyRP1R4CTHr3FeuDNfhsLPYrq2t0UBkUdK2jyTnXPEK1NQ",
+        crossorigin: "anonymous",
       },
     ],
-  ],
-  stylesheets: [
-    {
-      href: "https://cdn.jsdelivr.net/npm/katex@0.13.18/dist/katex.min.css",
-      type: "text/css",
-      integrity: "sha384-zTROYFVGOfTw7JV7KUu8udsvW2fx4lWOsCEDqhBreBwlHI4ioVRtmIvEThzJHGET",
-      crossorigin: "anonymous",
-    },
-  ],
-};
+  };
+}
+
+module.exports = config;
