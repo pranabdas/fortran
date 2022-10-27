9112dff1b9367b2e3d8386784d0c555e621e6fb7
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2022-10-27 12:34:20 +0000

    devcontainer settings, deploy build using node v18

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ async function config() {
     baseUrl: "/fortran/", // must have a trailing "/"
     onBrokenLinks: "throw",
     onBrokenMarkdownLinks: "warn",
-    favicon: "img/favicon.ico",
+    favicon: "img/favicon.png",
     organizationName: "pranabdas", // Usually your GitHub org/user name.
     projectName: "fortran", // Usually your repo name.
     titleDelimiter: "•",
@@ -42,7 +42,7 @@ async function config() {
           hideOnScroll: true,
           logo: {
             alt: "Logo",
-            src: "img/favicon.ico",
+            src: "img/favicon.png",
           },
           items: [
             {
